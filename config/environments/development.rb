47db595c2582068ee976d4908c8102f9421b7304
ventopreto <luiz.anjos@rebase.com.br>
2024-07-08 18:34:31 -0300

    adiciona autenticação com devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -21,9 +22,7 @@ Rails.application.configure do
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
+    config.public_file_server.headers = {'Cache-Control' => "public, max-age=#{2.days.to_i}"}
   else
     config.action_controller.perform_caching = false
 
