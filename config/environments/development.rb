3073a08882698c1c0fde071f9c59ac8365eb0444
Luiz Carlos <67896322+ventopreto@users.noreply.github.com>
2024-07-11 10:09:43 -0300

    adiciona autenticação com devise (#1)
    * adiciona autenticação com devise
    
    * adiciona nova pagina inicial
    
    ---------
    
    Co-authored-by: ventopreto <luiz.anjos@rebase.com.br>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,15 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+
+  config.sass.inline_source_maps = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -21,9 +24,7 @@ Rails.application.configure do
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
+    config.public_file_server.headers = {'Cache-Control' => "public, max-age=#{2.days.to_i}"}
   else
     config.action_controller.perform_caching = false
 
